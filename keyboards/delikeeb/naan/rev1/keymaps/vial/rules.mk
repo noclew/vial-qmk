3bc9456e0a8185385e1288c3ed2180312a3a0bd6
noclew <noclews@gmail.com>
2021-08-30 22:04:20 -0400

    naan vial firmware addded

--- keyboards/delikeeb/naan/rev1/keymaps/vial/rules.mk
+++ keyboards/delikeeb/naan/rev1/keymaps/vial/rules.mk
@@ -1,6 +1,10 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-AUDIO_ENABLE = no          # Audio output
 VIAL_ENCODERS_ENABLE = yes # enable rotary encoders
-TAP_DANCE_ENABLE = yes
+
+# disabled entities to reduce firmware size
+AUDIO_ENABLE = no          # Audio output
+TAP_DANCE_ENABLE = no
 QMK_SETTINGS = no
+COMBO_ENABLE = no
+MOUSEKEY_ENABLE = no
